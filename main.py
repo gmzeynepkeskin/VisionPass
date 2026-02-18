3717e0655641ebb08021e02455c7e9d85053dd93
gmzeynepkeskin <gm.zeynepkeskin@gmail.com>
2026-02-19 01:32:17 +0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
 from src.detection.detector import PersonDetector
+from src.tracking.tracker import PersonTracker
 
 def main():
     detector = PersonDetector()
+    tracker = PersonTracker()
     cap = cv2.VideoCapture(0)
 
     while True:
@@ -11,9 +13,17 @@ def main():
             break
 
         detections = detector.detect(frame)
+        tracks = tracker.update(frame, detections)
 
-        for (x1, y1, x2, y2) in detections:
+        for track_id, (x1, y1, x2, y2) in tracks:
+            x1, y1, x2, y2 = map(int, (x1, y1, x2, y2))
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
+            cv2.putText(frame, f"ID: {track_id}",
+                        (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.6,
+                        (0,255,0),
+                        2)
 
         cv2.imshow("VisionPass", frame)
 
@@ -25,3 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
